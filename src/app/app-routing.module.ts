bd93fc269a2533ae977ee7e0417f7f4a451052de
Amit Patange <amitpatange88@gmail.com>
2020-04-17 21:11:45 +0530

    DACSFront Login, Signup and Auth Service Implementation. (#1)
    * login signup and 404 pages designed
    
    * login changes updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent},
+  { path: 'Login', component: LoginComponent },
+  { path: 'Signup', component: SignupComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
