0a34051b1936c12d3aeebdee9a03a594869e8318
Amit Patange <amitpatange88@gmail.com>
2020-04-17 19:44:13 +0530

    login signup and 404 pages designed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+import { AuthService } from './services/auth.service';
+import {TokenInterceptorService} from './services/token-interceptor.service';
+import { StorageServiceModule } from 'ngx-webstorage-service';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    StorageServiceModule,
   ],
-  providers: [],
+  providers: [AuthService, 
+    {
+    provide:HTTP_INTERCEPTORS,
+    useClass: TokenInterceptorService,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
