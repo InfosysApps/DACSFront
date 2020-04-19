00c075d2fff5eaa6ebaea8071df7bf9630e9f1c9
Tanishq Gupta <tanishq.r.gupta@gmail.com>
2020-04-19 22:11:40 +0530

    Added basic customer information structure on dashboard

---
+++ src/app/models/customer.ts
@@ -0,0 +1,13 @@
+export class Customer {
+    name : string;
+    email : string;
+    contactNo : number;
+    assignedTo : string;
+
+    // constructor(name : string, email : string, contactNo : number, assignedTo : string) {
+    //     this.name = name;
+    //     this.email = email;
+    //     this.contactNo = contactNo;
+    //     this.assignedTo = assignedTo;
+    // }
+}
--- src/app/user-dashboard/user-dashboard.component.ts
+++ src/app/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from '../models/customer';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDashboardComponent implements OnInit {
 
+  customers : Customer[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getCustomers();
+    console.log(this.customers);
   }
 
+  getCustomers(): Customer[] {
+    for(let i = 0; i < 3; i++) {
+      let customer : Customer = new Customer();
+      customer.name = "Sachin Kothavade";
+      customer.email = "sachin@gmail.com";
+      customer.contactNo = 1234567890;
+      customer.assignedTo = "Amit Patange";
+      this.customers.push(customer);
+    }
+    return this.customers;
+  }
 }
