e97b051feadfd447c00d6b1fc12d774af3c6f302
komalkadam <komalkadam829@gmail.com>
2020-04-21 15:55:42 +0530

    adding pagination for sorting data and design table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { StorageServiceModule } from 'ngx-webstorage-service';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     ReactiveFormsModule,
     HttpClientModule,
     StorageServiceModule,
+    NgxPaginationModule,
   ],
   providers: [AuthService, 
     {
